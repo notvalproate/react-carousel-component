4b008e4a00a24692a03c9833dd409d40e24db370
Ian Tauro <sushruth127@gmail.com>
2022-12-19 00:07:26 +0530

    Fixed some issues, works normally now

--- src/App.js
+++ src/App.js
@@ -3,18 +3,20 @@ import Carousel from './components/Carousel';
 function App() {
 
   return (
-    <>
-      <Carousel items={[  
-        <img src="https://source.unsplash.com/user/erondu/800x450" alt="IMG"/>, 
+    <div style={{marginLeft: "1280px"}}>
+      <Carousel 
+      items={[
+        <div style={{width: "100%", padding: "5%"}}><h1>Item 1</h1><p>Hey guys this is a test if it works for divs</p></div>, 
         <div style={{width: "100%", padding: "5%"}}><h1>Item 2</h1><p>Hey guys this is a test if it works for divs</p></div>, 
-        <img src="https://source.unsplash.com/user/erondu/800x450" alt="IMG"/>,
+        <div style={{width: "100%", padding: "5%"}}><h1>Item 3</h1><p>Hey guys this is a test if it works for divs</p></div>, 
         <div style={{width: "100%", padding: "5%"}}><h1>Item 4</h1><p>Hey guys this is a test if it works for divs</p></div>, 
-        <img src="https://source.unsplash.com/user/erondu/800x450" alt="IMG"/>
+        <div style={{width: "100%", padding: "5%"}}><h1>Item 5</h1><p>Hey guys this is a test if it works for divs</p></div>, 
+        <div style={{width: "100%", padding: "5%"}}><h1>Item 6</h1><p>Hey guys this is a test if it works for divs</p></div> 
       ]}
       width={1280}
       height={720}
       />
-    </>
+    </div>
   );
 }
 
--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -3,7 +3,8 @@ import "./CarouselButtons.css"
 
 function Carousel(props) {
   const [activeIndex, setActiveIndex] = useState(0);
-  const [translateValue, setTranslateValue] = useState(0);
+  const [translateValue, setTranslateValue] = useState(-(100 / 3));
+  const [Items, setItems] = useState([props.items[props.items.length - 1], props.items[0], props.items[1]]);
 
   const carouselStyle = {
     position: "relative",
@@ -13,46 +14,61 @@ function Carousel(props) {
 
   const wrapperStyle = {
     display: "flex",
-    width: `${props.width * props.items.length}px`,
+    width: `${props.width * 3}px`,
     height: "100%",
     transform: `translateX(${translateValue}%)` 
   }
 
   const itemStyle = {
     display: "inline-flex",
+    overflow: "hidden",
     justifyContent: "center",
     height: `${props.height}px`,
     width: `${props.width}px`
   }
 
+  const getList = (i) => {
+    let middle = i < 0 ? props.items.length - 1 : i % props.items.length;
+    let first = (middle - 1) < 0 ? props.items.length - 1 : middle - 1;
+    let last = (i + 1) % props.items.length;
+  
+    const array = [
+      props.items[first],
+      props.items[middle],
+      props.items[last]
+    ];
+    
+    return array;
+  }
+
   const nextItem = () => {
-    if (activeIndex === props.items.length - 1) {
+    setItems(getList(activeIndex + 1));
+
+    if (activeIndex === (props.items.length - 1)) {
       setActiveIndex(0);
-      setTranslateValue(0);
     } 
     else {
       setActiveIndex(activeIndex + 1);
-      setTranslateValue(translateValue - (100 / props.items.length));
     }
   }
 
   const prevItem = () => {
+    setItems(getList(activeIndex - 1));
+
     if (activeIndex === 0) {
       setActiveIndex(props.items.length - 1);
-      setTranslateValue(-100 + (100 / props.items.length));
     } 
     else {
       setActiveIndex(activeIndex - 1);
-      setTranslateValue(translateValue + (100 / props.items.length));
     }
   }
 
   return (
     <div className="carousel" style={carouselStyle}>
       <div className="carousel-wrapper" style={wrapperStyle}>
-        {props.items.map((item) => {
+        {Items.map((item, index) => {
           return (
-            <div style={itemStyle}>
+            <div key={index} style={itemStyle}>
               {item}
             </div>
           )
